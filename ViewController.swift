97561e5be4304c715a0a603499dcf3e22814ffd3
anurodh30 <93106600+anurodh30@users.noreply.github.com>
2022-10-29 17:20:33 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,15 @@
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
