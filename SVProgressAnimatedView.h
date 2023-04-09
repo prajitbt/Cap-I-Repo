d9b3171c34e0552bc59c7d502a3439aec5fc9c3d
PrabinGhaley <116981691+PrabinGhaley@users.noreply.github.com>
2023-04-08 22:47:42 -0500

    SV progress animated header

---
+++ SVProgressAnimatedView.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface SVProgressAnimatedView : UIView
+
+@property (nonatomic, assign) CGFloat radius;
+@property (nonatomic, assign) CGFloat strokeThickness;
+@property (nonatomic, strong) UIColor *strokeColor;
+@property (nonatomic, assign) CGFloat strokeEnd;
+
+@end
