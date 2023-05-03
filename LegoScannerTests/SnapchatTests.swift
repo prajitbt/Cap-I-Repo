e2edad4b8323fd9d1182bee13cf648fd124a366f
prajitbt <61091969+prajitbt@users.noreply.github.com>
2023-05-03 13:55:42 -0500

    Update SnapchatTests.swift

--- LegoScannerTests/SnapchatTests.swift
+++ LegoScannerTests/SnapchatTests.swift
@@ -1,9 +1,4 @@
-//
-//  SnapchatTests.swift
-//  SnapchatTests
-//
-//  Created by Suman on 3/27/23.
-//
+
 
 import XCTest
 @testable import Snapchat
@@ -22,8 +17,7 @@ final class SnapchatTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+
     }
 
     func testPerformanceExample() throws {
